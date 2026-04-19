8b07ca34fd33eab59994436cafbf7a4a1fa823a4
Hidden-Node <mr.regista6@gmail.com>
2026-04-19 21:16:48 +0330

    fix(android): Force app layout direction to LTR regardless of system locale

--- android/app/src/main/java/com/masterdns/vpn/MainActivity.kt
+++ android/app/src/main/java/com/masterdns/vpn/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.platform.LocalLayoutDirection
 import com.masterdns.vpn.ui.navigation.AppNavigation
 import com.masterdns.vpn.ui.theme.MasterDnsVPNTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,8 +17,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MasterDnsVPNTheme {
-                AppNavigation()
+            CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
+                MasterDnsVPNTheme {
+                    AppNavigation()
+                }
             }
         }
     }
