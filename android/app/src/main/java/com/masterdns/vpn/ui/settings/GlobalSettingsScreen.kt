df942564758e30a545ba8a8c0fe710bd80da3ad1
Hidden-Node <mr.regista6@gmail.com>
2026-05-04 01:58:27 +0330

    fix(ui): add missing Compose imports for split-tunneling dialog scroll/sticky footer build

--- android/app/src/main/java/com/masterdns/vpn/ui/settings/GlobalSettingsScreen.kt
+++ android/app/src/main/java/com/masterdns/vpn/ui/settings/GlobalSettingsScreen.kt
@@ -2,6 +2,8 @@ package com.masterdns.vpn.ui.settings
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
@@ -12,6 +14,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
